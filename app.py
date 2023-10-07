071b5a66e2ba6aabd9fe2a98998d9c99d82ab483
Jay Mehta <mehtajay096@gmail.com>
2023-10-07 15:53:32 +0530

    final commit to code

--- app.py
+++ app.py
@@ -80,15 +80,15 @@ def main():
 
 if __name__ == '__main__':
     main()
-import requests
+# import requests
 
-API_URL = "https://api-inference.huggingface.co/models/openai/whisper-large-v2"
-headers = {"Authorization": "Bearer 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"}
+# API_URL = "https://api-inference.huggingface.co/models/openai/whisper-large-v2"
+# headers = {"Authorization": "Bearer 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"}
 
-def query(filename):
-    with open(filename, "rb") as f:
-        data = f.read()
-    response = requests.post(API_URL, headers=headers, data=data)
-    return response.json()
+# def query(filename):
+#     with open(filename, "rb") as f:
+#         data = f.read()
+#     response = requests.post(API_URL, headers=headers, data=data)
+#     return response.json()
 
-output = query("sample1.flac")
+# output = query("sample1.flac")
